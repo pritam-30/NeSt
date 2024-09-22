30e2ef51aac953ebfb66419c8e62d4e1f8bc932f
pritam-30 <bhowmikpritam2016@gmail.com>
2024-09-22 20:26:59 +0530

    middleware added

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,19 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/logIn" || path === "/signUP";
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/profile", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/logIn", request.nextUrl));
+  }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/profile", "/signUP", "logIn"],
+  matcher: ["/", "/profile", "/signUP", "/logIn"],
 };
